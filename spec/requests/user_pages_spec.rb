a6fcc098a073cbd9650b4279a5801e15e565bbec
Douglas Aguiar <douglasoa@gmail.com>
2013-07-02 00:29:16 -0300

    Ecercícios do capítulo 9 finalizados

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -41,12 +41,7 @@ describe "User pages" do
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
-      end
+      before { valid_signup } 
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
@@ -57,7 +52,7 @@ describe "User pages" do
         let(:user) { User.find_by_email('user@example.com') }
 
         it { should have_selector('title', text: user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_welcome_message('Welcome') }
         it { should have_link('Sign out') }
       end
     end
